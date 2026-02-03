7934322d770680a98fba89f8e279c2c838414a12
Marta Solomacha <marciamarcia@o2.pl>
2026-02-03 10:50:19 +0100

    handle vue-router config and base pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <h1>Home Page</h1>
+  <RouterLink to="/projects">Projects Page</RouterLink>
 </template>
 
 <style scoped></style>
