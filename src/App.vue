70a4a7ef20fdbb93e2144e8a4d2d9b3d642ff7ca
Marta Solomacha <marciamarcia@o2.pl>
2026-02-05 17:24:18 +0100

    Add iconify-icon and serach icon

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,11 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 <template>
   <nav class="h-16 border-b bg-muted-400 flex items-center justify-between px-6">
-    <form class="w-full max-w-96">
+    <form class="relative w-full h-fit max-w-96">
+      <iconify-icon
+        icon="lucide:search"
+        class="absolute top-1/2 -translate-y-1/2 left-2.5 text-muted-foreground"
+      />
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
 
