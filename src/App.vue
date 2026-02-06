bfe5d88caf6e3a44e07f0a3d3c637220842e186c
Marta Solomacha <marciamarcia@o2.pl>
2026-02-06 10:44:33 +0100

    Add sidebar an layout

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,9 @@
 <script setup lang="ts">
-import Input from '@/components/ui/input/Input.vue'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import AuthLayout from '@/components/Layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted-400 flex items-center justify-between px-6">
-    <form class="relative w-full h-fit max-w-96">
-      <iconify-icon
-        icon="lucide:search"
-        class="absolute top-1/2 -translate-y-1/2 left-2.5 text-muted-foreground"
-      />
-      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
-    </form>
-
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
+  <AuthLayout>
     <RouterView />
-  </main>
+  </AuthLayout>
 </template>
-
-<style scoped></style>
---
+++ src/types/layout.ts
@@ -0,0 +1,5 @@
+export interface LinkProp {
+  title: string
+  icon: string
+  to: string
+}
