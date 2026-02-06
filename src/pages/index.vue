78f0f85d49ed6258aeea0d9b58eb6f9068f5da84
Marta Solomacha <marciamarcia@o2.pl>
2026-02-06 15:14:43 +0100

    Add tanstack package and create tables

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Home Page</h1>
-  <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Project 1 Page</RouterLink>
 </template>
 
 <style scoped></style>
---
+++ src/pages/projects/[slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute('/projects/[slug]')
+const slug = computed(() => route.params?.slug as string)
+</script>
+
+<template>
+  <h1>Project slug:</h1>
+  <h1>{{ slug }}</h1>
+</template>
