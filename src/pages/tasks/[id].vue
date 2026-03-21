41b39f3a5ce8329e8ed8847e4e3f8cfe769afc91
MartaSołomacha <marciamarcia@o2.pl>
2026-03-21 19:13:07 +0100

    feat/make-tasks-editable (#31)

--- src/pages/tasks/[id].vue
+++ src/pages/tasks/[id].vue
@@ -1,68 +1,71 @@
 <script setup lang="ts">
-import { taskQuery } from '@/utils/supaQueries'
-import type { Task } from '@/utils/supaQueries'
+const { id } = useRoute('/tasks/[id]').params
 
-const route = useRoute('/tasks/[id]')
-
-const task = ref<Task | null>(null)
-
-const { setError } = useErrorStore()
+const tasksLoader = useTasksStore()
+const { task } = storeToRefs(tasksLoader)
+const { getTask, updateTask } = tasksLoader
 
 watch(
   () => task.value?.name,
   () => {
-    usePageStore().pageData.title = task.value?.name ? `Task ${task.value.name}` : ''
+    usePageStore().pageData.title = `Task: ${task.value?.name || ''}`
   },
 )
 
-const getTask = async () => {
-  const { data, error, status } = await taskQuery(route.params.id)
-  if (error) if (error) setError({ error, customCode: status })
-  task.value = data
-}
+await getTask(id)
 
-await getTask()
+const { getProfilesByIds } = useCollabs()
+
+const collabs = task.value?.collaborators ? await getProfilesByIds(task.value?.collaborators) : []
 </script>
 
 <template>
   <Table v-if="task">
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell> {{ task.name }} </TableCell>
+      <TableCell>
+        <AppInPlaceEditText v-model="task.name" @commit="updateTask" />
+      </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
       <TableCell>
-        {{ task.description }}
+        <AppInPlaceEditTextarea class="h-20" v-model="task.description" @commit="updateTask" />
       </TableCell>
     </TableRow>
     <TableRow>
-      <TableHead> Assignee </TableHead>
-      <TableCell>Lorem ipsum</TableCell>
-    </TableRow>
-    <TableRow>
       <TableHead> Project </TableHead>
-      <TableCell> {{ task.projects?.name }} </TableCell>
+      <TableCell>{{ task.projects?.name }}</TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Status </TableHead>
-      <TableCell> {{ task.status }} </TableCell>
+      <TableCell>
+        <AppInPlaceEditStatus v-model="task.status" @commit="updateTask" />
+      </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Collaborators </TableHead>
       <TableCell>
-        <div class="flex">
-          <Avatar
-            class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            v-for="collaborator in task.collaborators"
-            :key="collaborator"
-          >
-            <RouterLink class="w-full h-full flex items-center justify-center" to="">
-              <AvatarImage src="" alt="" />
-              <AvatarFallback> </AvatarFallback>
-            </RouterLink>
-          </Avatar>
-        </div>
+        <TableCell>
+          <div class="flex">
+            <Avatar
+              class="-mr-4 border border-primary hover:scale-110 transition-transform"
+              v-for="collab in collabs"
+              :key="collab.id"
+            >
+              <RouterLink
+                class="w-full h-full flex items-center justify-center"
+                :to="{
+                  name: '/users/[username]',
+                  params: { username: collab.username },
+                }"
+              >
+                <AvatarImage :src="collab.avatar_url || ''" alt="" />
+                <AvatarFallback> </AvatarFallback>
+              </RouterLink>
+            </Avatar>
+          </div>
+        </TableCell>
       </TableCell>
     </TableRow>
     <TableRow class="hover:bg-transparent">
@@ -97,4 +100,18 @@ await getTask()
   </Table>
 </template>
 
-<style scoped></style>
+<style>
+@reference '../../assets/style.css';
+
+th {
+  @apply w-[100px];
+}
+
+h2 {
+  @apply mb-4 text-lg font-semibold w-fit;
+}
+
+.table-container {
+  @apply overflow-hidden overflow-y-auto rounded-md bg-slate-900 h-80;
+}
+</style>
--- src/utils/tableColumns/tasksColumns.ts
+++ src/utils/tableColumns/tasksColumns.ts
@@ -1,8 +1,13 @@
-import { RouterLink } from 'vue-router'
-import type { TaskWithProject } from '../supaQueries'
 import type { ColumnDef } from '@tanstack/vue-table'
+import type { TaskWithProject } from '../supaQueries'
+import { RouterLink } from 'vue-router'
+import type { GroupedCollabs } from '@/types/groupedCollabs'
+import Avatar from '@/components/ui/avatar/Avatar.vue'
+import AvatarImage from '@/components/ui/avatar/AvatarImage.vue'
+import AvatarFallback from '@/components/ui/avatar/AvatarFallback.vue'
+import AppInPlaceEditStatus from '@/components/AppInPlaceEdit/AppInPlaceEditStatus.vue'
 
-export const columns: ColumnDef<TaskWithProject[0]>[] = [
+export const columns = (collabs: Ref<GroupedCollabs>): ColumnDef<TaskWithProject[0]>[] => [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -10,10 +15,10 @@ export const columns: ColumnDef<TaskWithProject[0]>[] = [
       return h(
         RouterLink,
         {
-          to: `tasks/${row.original.id}`,
+          to: `/tasks/${row.original.id}`,
           class: 'text-left font-medium hover:bg-muted block w-full',
         },
-        () => h('span', row.getValue('name')),
+        () => row.getValue('name'),
       )
     },
   },
@@ -21,7 +26,14 @@ export const columns: ColumnDef<TaskWithProject[0]>[] = [
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        h(AppInPlaceEditStatus, {
+          modelValue: row.original.status,
+          readonly: true,
+        }),
+      )
     },
   },
   {
@@ -39,10 +51,10 @@ export const columns: ColumnDef<TaskWithProject[0]>[] = [
         ? h(
             RouterLink,
             {
-              to: `projects/${row.original.projects?.slug}`,
+              to: `/projects/${row.original.projects.slug}`,
               class: 'text-left font-medium hover:bg-muted block w-full',
             },
-            () => h('span', row.original.projects?.name),
+            () => row.original.projects?.name,
           )
         : ''
     },
@@ -53,8 +65,18 @@ export const columns: ColumnDef<TaskWithProject[0]>[] = [
     cell: ({ row }) => {
       return h(
         'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators')),
+        { class: 'text-left font-medium h-20 flex items-center' },
+        collabs.value[row.original.id]
+          ? collabs.value[row.original.id].map((collab) => {
+              return h(RouterLink, { to: `/users/${collab.username}` }, () => {
+                return h(Avatar, { class: 'hover:scale-110 transition-transform' }, () =>
+                  h(AvatarImage, { src: collab.avatar_url || '' }),
+                )
+              })
+            })
+          : row.original.collaborators.map(() => {
+              return h(Avatar, { class: 'animate-pulse' }, () => h(AvatarFallback))
+            }),
       )
     },
   },
