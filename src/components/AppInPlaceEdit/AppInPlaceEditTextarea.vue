200c82c0f20935ad397850948f54858dbd3bfdbb
Marta Solomacha <marciamarcia@o2.pl>
2026-03-21 18:26:10 +0100

    Make the description input and adjust database schema

---
+++ src/components/AppInPlaceEdit/AppInPlaceEditTextarea.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const value = defineModel<string>()
+
+defineEmits<{
+  (e: 'commit'): void
+}>()
+</script>
+
+<template>
+  <textarea
+    class="w-full p-1 bg-transparent focus:outline-none focus:border-none focus:bg-gray-800 focus:rounded-md"
+    v-model="value"
+    @blur="$emit('commit')"
+  />
+</template>
+
+<style scoped></style>
