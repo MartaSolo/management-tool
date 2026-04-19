f269d12507d909b7eb21acf60cb1f8306b5f234c
MartaSołomacha <marciamarcia@o2.pl>
2026-04-19 21:44:30 +0200

    feat/add-page-transition (#38)

--- src/components/Layout/main/AuthLayout.vue
+++ src/components/Layout/main/AuthLayout.vue
@@ -16,16 +16,21 @@ provide(menuKey, {
 </script>
 
 <template>
-  <Sidebar @task-clicked="taskSheetOpen = true" />
-  <div class="flex flex-col transition-[margin]" :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }">
-    <TopNavbar />
-    <AppNewTask v-model="taskSheetOpen" />
+  <div>
+    <Sidebar @task-clicked="taskSheetOpen = true" />
+    <div
+      class="flex flex-col transition-[margin]"
+      :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }"
+    >
+      <TopNavbar />
+      <AppNewTask v-model="taskSheetOpen" />
 
-    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
-      </div>
-      <slot />
-    </main>
+      <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
+        </div>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
